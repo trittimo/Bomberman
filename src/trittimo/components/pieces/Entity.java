2bc166fa04de0b65a197bd8547718c7c742112db
Andrew McKee <mckeeaj@rose-hulman.edu>
2016-11-06 00:26:22 -0400

    updated entity
    wew lads

--- src/trittimo/components/pieces/Entity.java
+++ src/trittimo/components/pieces/Entity.java
@@ -10,12 +10,27 @@ public abstract class Entity extends JComponent {
 	
 	public Screen screen;
 	
+	public Entity(Screen s, int xp, int yp) {
+		this.screen = s;
+		this.x = xp;
+		this.y = yp;
+		this.setLocation(xp * Screen.GRID_SIZE, yp * Screen.GRID_SIZE);
+	}
+	
 	public Entity(Screen screen) {
 		this.screen = screen;
 	}
 	
-	public boolean attemptMove(int x, int y) {
-		
-		return false;
+	public boolean attemptMove(int ax, int ay) {
+		//check if occupied
+		for (Entity e: screen.entities) {
+			if ((e.x == ax) && (e.y == ay))
+				if ((e instanceof Enemy) || (e instanceof DestructableWall) || (e instanceof Wall))
+					return false;
+		}
+		this.x = ax;
+		this.y = ay;
+		this.setLocation(x * Screen.GRID_SIZE, y * Screen.GRID_SIZE);
+		return true;
 	}
 }
