9d71f371a1d6b09e5139f7fd7a437c6b4f909d3d
gilderjw <gilderjw@rose-hulman.edu>
2016-11-06 00:29:29 -0400

    fisrt try at bomb jank

--- src/trittimo/components/pieces/Bomb.java
+++ src/trittimo/components/pieces/Bomb.java
@@ -1,11 +1,41 @@
 package trittimo.components.pieces;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import trittimo.components.Screen;
 
-public class Bomb extends Entity {
 
-	public Bomb(Screen screen) {
-		super(screen);
+
+public class Bomb extends Entity {
+	public static final int SECONDS_TO_BLOW = 5;
+	public static final int SECONDS_TO_MILI = 60*1000;
+	Timer timer;
+	Bomb ths = this;
+
+	public Bomb(Screen screen, int x, int y) {
+		super(screen, x, y);
+		this.timer = new Timer();
+
+		this.timer.schedule(new TimerTask(){
+			@Override
+			public void run() {
+				Bomb.this.ths.explode();
+			}
+		}, Bomb.SECONDS_TO_BLOW * Bomb.SECONDS_TO_MILI);
+
 	}
 
+	public void explode(){
+		System.out.println("boom");;
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.BLACK);
+		g.drawOval(this.x, this.y, 50, 50);
+	}
 }
