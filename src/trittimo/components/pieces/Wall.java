2f41d2bccf8e2f092d012a38a2de558acef7278b
decramrj <decramrj@rose-hulman.edu>
2016-11-06 00:20:55 -0400

    Did stuff to wall

--- src/trittimo/components/pieces/Wall.java
+++ src/trittimo/components/pieces/Wall.java
@@ -1,11 +1,21 @@
 package trittimo.components.pieces;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import trittimo.components.Screen;
 
 public class Wall extends Entity {
 
 	public Wall(Screen screen) {
 		super(screen);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawRect(this.x, this.y, 50, 50);
+		g.setColor(Color.CYAN);
 	}
 
 }
